2102a2c1465b8a6a0c180116296984e22a2466ec
liam <liam@ldr-dev>
2017-07-22 12:48:29 -0400

    added a phrase, corrected word list

--- phrases.py
+++ phrases.py
@@ -1,23 +1,48 @@
 from hds_setup import *
 from insult import *
+from random import random
 
 
-p1 = insult("{}! Why haven't you taken a shower yet you {} {}!?\n",
+insults = []
+
+p1 = insult("{}! Why haven't you taken a shower yet you {} {}!?",
             [adhominem, adjectives, animals])
+insults.append(p1)
 
-p2 = insult("Yeah, you {} {}, I {} your mother while riding a {}.\n",
+p2 = insult("Yeah, you {} {}, I {} your mother while riding a {}.",
             [adjectives, rude_nouns, past_transitive, animals])
+insults.append(p1)
+
+p3 = insult("I would rather {} a {} than spend another minute with you, you {} {}",
+            [infinitive_transitive, animals, adjectives, rude_nouns])
+insults.append(p3)
+
+
+p4 = insult("Holy {}! You are the sinlge most {} piece of {} that I have ever seen",
+            [rude_nouns, adjectives, rude_nouns])
+insults.append(p4)
 
 
 
-print(p1.fill())
-print(p2.fill())
 
-print(p1.fill())
-print(p2.fill())
 
-print(p1.fill())
-print(p2.fill())
 
-print(p1.fill())
-print(p2.fill())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def random_phrase():
+    print(insults[int(random() * len(insults))].fill())
+
+if __name__ == "__main__":
+   random_phrase()
