16eb8459af9cd08154b39260d889375d1f7d31e2
liam <liam@ldr-dev>
2017-07-22 09:15:39 -0400

    create and add hds.py (for real this time)

---
+++ hds.py
@@ -0,0 +1,41 @@
+from random import random
+
+# Hierarchical data structure
+class hds:
+
+    recursion_depth = 0
+
+    def add_child(self, child):
+        self.children.append(child)
+    def add_parent(self, parent):
+        self.parents.append(parent)
+    def add_data(self, element):
+        self.data.append(element)
+
+    def random(self, include_descendants_in_search = False):
+        if include_descendants_in_search == False or len(self.children) == 0:
+            if len(self.data) == 0:
+                return None
+            return self.data[int(random() * len(self.data))]
+        else:
+            hds.recursion_depth += 1
+            possibilities = [self.random()]
+            for child in self.children:
+                possibilities.append(child.random(True))
+
+            if hds.recursion_depth == 1:
+                hds.recursion_depth = 0
+                return possibilities[int(random() * len(possibilities))]
+
+            hds.recursion_depth -= 1
+            return possibilities
+
+    def __init__(self, parent = None):
+        self.data = []
+        self.parents = []
+        self.children = []
+        if parent != None:
+            print("Parent ", parent)
+            print("self ", self)
+            self.add_parent(parent)
+            parent.add_child(self)
