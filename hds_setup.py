c24f7bf3f0a2ec780add64cfcc1d7907922a1b5d
liam <liam@ldr-dev>
2017-07-22 15:23:28 -0400

    various fixes

--- hds_setup.py
+++ hds_setup.py
@@ -9,12 +9,22 @@ verbs = hds(root)
 
 adjectives = hds(root)
 adjectives.data_from_file("wordlists/curses/adjectives.txt")
+adjectives.data_from_file("wordlists/curses/adjectives2.txt")
 
 animals = hds(nouns)
 animals.data_from_file("wordlists/animals.txt")
 
+family = hds(nouns)
+family.data_from_file("wordlists/family.txt")
+
+body_parts = hds(nouns)
+body_parts.data_from_file("wordlists/body_parts.txt")
+
 rude_nouns = hds(nouns)
 rude_nouns.data_from_file("wordlists/misc/bad-nouns.txt")
+
+abstract_nouns = hds(nouns)
+abstract_nouns.data_from_file("wordlists/abstract_nouns.txt")
 
 adhominem = hds(exclamations)
 adhominem.data_from_file("wordlists/curses/adhominem.txt")
--- phrases.py
+++ phrases.py
@@ -6,17 +6,17 @@ from random import random
 insults = []
 
 insults.append(
-    insult("{}! Why haven't you taken a shower yet you {} {}!?",
-            [adhominem, adjectives, animals])
+    insult("You smell like {} {}s; Why haven't you taken a shower yet you {}!?",
+            [adjectives, animals, rude_nouns])
 )
 
 insults.append(
-    insult("Yeah, you {} {}, I {} your mother while riding a {}.",
-            [adjectives, rude_nouns, past_transitive, animals])
+    insult("Yeah you {} {}, I {} your {} while riding a {}.",
+            [adjectives, rude_nouns, past_transitive, family, animals])
 )
 
 insults.append(
-    insult("I would rather {} a {} than spend another minute with you, you {} {}",
+    insult("I would rather {} a {} than spend another minute with you, you {} {}.",
             [infinitive_transitive, animals, adjectives, rude_nouns])
 )
 
@@ -35,15 +35,15 @@ insults.append(
            [progressive_transitive, animals, adjectives, rude_nouns])
 )
 
+insults.append(
+    insult("How can I put into words what you mean to me? ... Oh wait, I got it: {} {}.",
+            [adjectives, rude_nouns])
+)
 
-
-
-
-
-
-
-
-
+insults.append(
+    insult("Every time I see your {}, I weep in {} for what a {} place you've made the world.",
+            [body_parts, abstract_nouns, adjectives])
+)
 
 
 
@@ -54,7 +54,8 @@ insults.append(
 
 
 def random_phrase():
-    print(insults[int(random() * len(insults))].fill())
+    r = int(random() * len(insults))
+    print(insults[r].fill())
 
 if __name__ == "__main__":
    random_phrase()
