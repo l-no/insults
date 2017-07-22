afe383230b5a877cc303a623368f5f4c86eab0ba
liam <liam@ldr-dev>
2017-07-22 09:46:45 -0400

    basic insult structure added

--- insult.py
+++ insult.py
@@ -1,3 +1,31 @@
 from hds import hds
 
+class insult:
+    def fill(self):
+        args = [x.random() for x in self.categories]
+        return self.phrase.format(*args)
+    def __init__(self, phrase, categories = None):
+        self.phrase = phrase
+        self.categories = categories
+
+
+animals = hds()
+with open("animals.txt", "r") as f:
+    for animal in f:
+        animals.add_data(animal)
+
+
+x = insult("My favorite animal is the majestic {}",[animals])
+
+print(x.fill())
+print(x.fill())
+print(x.fill())
+print(x.fill())
+print(x.fill())
+print(x.fill())
+print(x.fill())
+print(x.fill())
+print(x.fill())
+print(x.fill())
+
 
