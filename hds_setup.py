8f27b4e553beca7db6872cc40550634d442e0968
liam <liam@ldr-dev>
2017-07-22 12:02:23 -0400

    initial phrases

--- hds_setup.py
+++ hds_setup.py
@@ -6,6 +6,8 @@ root = hds()
 nouns = hds(root)
 exclamations = hds(root)
 
+adjectives = hds(root)
+adjectives.data_from_file("wordlists/curses/adjectives.txt")
 
 animals = hds(nouns)
 animals.data_from_file("wordlists/animals.txt")
---
+++ phrases.py
@@ -0,0 +1,17 @@
+from hds_setup import *
+from insult import *
+
+
+p1 = insult("{}! Why haven't you taken a shower yet you {} {}!?",
+            [adhominem, adjectives, animals])
+
+
+
+
+
+print(p1.fill())
+print(p1.fill())
+print(p1.fill())
+print(p1.fill())
+print(p1.fill())
+print(p1.fill())
